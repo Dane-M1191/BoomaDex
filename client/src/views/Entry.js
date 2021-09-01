22af44364056084eaa0bdeb0ecfa438045a2994c
booma1191 <booma1191.m@gmail.com>
2021-09-01 06:39:04 -0500

    attempting to get images to load

--- client/src/views/Entry.js
+++ client/src/views/Entry.js
@@ -9,6 +9,29 @@ module.exports = {
             Pokemon.setPokemon(window.location.href.split("/")[5])
         }
 
-        return m("h1", Pokemon.name)
+        // return m("h1", Pokemon.name)
+        //--------------------------------shows correct name-----------------------------------
+
+
+        return m(".entry-row",
+            m(".entry-col2",
+                m(".entry-row", Pokemon.name + " #" + window.location.href.split("/")[5]),
+                m(".entry-row"),
+                m(".entry-row"),
+                m(".entry-row"),
+            ), m(".entry-col-img", 
+                m(".entry-row", 
+                    m(".entry-col", "Normal"), 
+                    m(".entry-col", "Shiny")
+                ), 
+                m(".entry-row", 
+                    // m(".entry-col-img2", m("img", {src: "https://i.imgur.com/qETfuqv.png", alt: "bulba"})), 
+                    m(".entry-col-img2", m("img", { src: "./test.png", alt: "awdawd",width: '100', height: '200' })),
+                    m(".entry-col-img2")
+                )
+            )
+                
+            
+        )
     }
 }
